9bc26cf15d0a411fe40aa18a40de8eface738a9a
Mustafa Abusharkh <dotachessfan@outlook.com>
2021-10-16 14:36:56 -0500

    added connect page

--- src/components/home/Connect.js
+++ src/components/home/Connect.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import contact from './contact.jpg'
+import { GrFacebook, GrInstagram } from 'react-icons/gr'
+import ButtonMailto from './ButtonMailto'
 const Connect = () => {
-    return (
-        <div>
-            I am mister connect
-        </div>
-    )
+  return (
+    <div>
+      <img src={contact} style={{ width: '100%', height: '400px' }} />
+      <br></br>
+      <br></br>
+      <p style={{ textAlign: 'right' }}>
+        <ButtonMailto
+          label='masa7tee@gmail.com'
+          mailto='mailto:masa7tee@gmail.com'
+        />
+        <span>البريد الالكتروني</span>
+      </p>
+      <p style={{ textAlign: 'right' }}>
+        <a href='https://www.facebook.com/Masa7tee'>
+          <GrFacebook /> https://www.facebook.com/Masa7tee
+        </a>
+        عن طريق صفحة الفيسبوك
+      </p>
+      <p style={{ textAlign: 'right' }}>
+        <a href='https://www.instagram.com/masa7tee'>
+          <GrInstagram /> https://www.instagram.com/masa7tee
+        </a>
+        عن طريق صفحة الإنستغرام
+      </p>
+    </div>
+  )
 }
 
 export default Connect
